cb4a39a0c5b10e90918cadd88c54ed9a73895f9d
Nirdosh4096 <91086417+Nirdosh1024@users.noreply.github.com>
2023-08-05 14:51:40 +0530

    Site A Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class ProductController extends Controller
 {
@@ -18,4 +19,12 @@ class ProductController extends Controller
             "product" => $product
         ]);
     } 
+
+    public function checkout(Request $request) {
+        $id = $request->query("id");
+        $mg = $request->query("mg");
+        $qty = $request->query("qty");
+
+        return Redirect::away("http://localhost:4000/pay?id=$id&mg=$mg&qty=$qty");
+    }
 }
