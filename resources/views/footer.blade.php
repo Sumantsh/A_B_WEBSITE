646ff04f2ddfe70ee485e0943f78763dd2edbd7b
Nirdosh4096 <91086417+Nirdosh1024@users.noreply.github.com>
2023-08-10 17:21:28 +0530

    code updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -38,6 +38,7 @@
   
 <script src=" https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+<script src="{{ asset('js/script.js') }}"></script>
 <script src="{{ asset('js/swiperscript.js') }}"></script>
 <script src="{{ asset('js/cartwindow.js') }}"></script>
 
